6b5be6ae946cd13443626db173d92b8b44c28dae
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-04-17 16:32:44 +0300

    fix bug and remove all style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Button from "./component/Button.jsx";
+
 function App() {
-  return <div className="App">alzubair</div>;
+  return (
+    <div className="App">
+      <div>
+        <Button />
+      </div>
+    </div>
+  );
 }
 
 export default App;
