4ede254d4ace7fe44e3355b3911c799c4cb344c2
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-04-25 13:35:18 +0300

    allow the classname

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -17,22 +17,26 @@ const Button = ({
   outred,
   ...rest
 }) => {
-  const classes = className("flex items-center gap-1 px-3 py-1.5 border", {
-    "bg-blue-500 broder-blue-500 text-white": primary,
-    "bg-gray-900 broder-gray-900 text-white": secondary,
-    "bg-green-500 broder-green-500 text-white": success,
-    "bg-yellow-400 broder-yellow-400 text-white": warning,
-    "bg-red-500 broder-red-500 text-white": danger,
-    // rounded
-    "rounded-full": rounded,
-    // outline
-    "bg-white": outline,
-    "text-blue-500 border-blue-500": outblue,
-    "text-gray-900 border-gray-900": outblack,
-    "text-green-500 border-green-500": outgreen,
-    "text-yellow-400 border-yellow-400": outyellow,
-    "text-red-500 border-red-500": outred,
-  });
+  const classes = className(
+    rest.className,
+    "flex items-center gap-1 px-3 py-1.5 border",
+    {
+      "bg-blue-500 broder-blue-500 text-white": primary,
+      "bg-gray-900 broder-gray-900 text-white": secondary,
+      "bg-green-500 broder-green-500 text-white": success,
+      "bg-yellow-400 broder-yellow-400 text-white": warning,
+      "bg-red-500 broder-red-500 text-white": danger,
+      // rounded
+      "rounded-full": rounded,
+      // outline
+      "bg-white": outline,
+      "text-blue-500 border-blue-500": outblue,
+      "text-gray-900 border-gray-900": outblack,
+      "text-green-500 border-green-500": outgreen,
+      "text-yellow-400 border-yellow-400": outyellow,
+      "text-red-500 border-red-500": outred,
+    }
+  );
 
   return (
     <button {...rest} className={classes}>
