d3e5619594139f062cdb7ed1c39e642afda38b8a
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-04-25 16:21:28 +0300

    create the accordion

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import ButtonPage from "./pages/ButtonPage.jsx";
+import Accordion from "./component/Accordion.jsx";
 
 function App() {
+  const items = [
+    {
+      label: "can i use angular",
+      content: "no you cant",
+    },
+    {
+      label: "can i use React",
+      content: "yes you can",
+    },
+    {
+      label: "can i use vue",
+      content: "no you cant",
+    },
+  ];
   return (
     <div className="App">
-      <ButtonPage />
+      <Accordion />
     </div>
   );
 }
