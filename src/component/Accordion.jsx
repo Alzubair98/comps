1d8b5c1f435a0d81f2527255a4e4967b579a3cfb
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-04-27 19:36:05 +0300

    use short and long hand function

--- src/component/Accordion.jsx
+++ src/component/Accordion.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
   const [expandedIndex, setExpandedIndex] = useState(0);
+  const handleClick = (index) => {
+    setExpandedIndex(index);
+  };
   return (
     <div>
       {items.map((item, index) => {
@@ -9,13 +12,7 @@ const Accordion = ({ items }) => {
 
         return (
           <div key={index}>
-            <div
-              onClick={() => {
-                setExpandedIndex(index);
-              }}
-            >
-              {item.label}{" "}
-            </div>
+            <div onClick={() => handleClick(index)}>{item.label} </div>
             {isExpanded && <div>{item.content} </div>}
           </div>
         );
