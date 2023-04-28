6868b5a28ef9b6a363f4d492d338ef12239fc173
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-04-28 22:44:28 +0300

    first try for drop down

--- src/component/Dropdown.jsx
+++ src/component/Dropdown.jsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Dropdown = ({ options }) => {
-  console.log(options);
-  return <div>im drop down nigga</div>;
+  const [down, setDown] = useState(0);
+  const [value, setValue] = useState("select...");
+  const TrueDown = down === 1;
+
+  return (
+    <>
+      <div
+        onClick={() => {
+          down === 1 ? setDown(0) : setDown(1);
+        }}
+      >
+        {value}
+      </div>
+
+      {TrueDown && (
+        <div>
+          {options.map((option, index) => {
+            return (
+              <div
+                key={index}
+                onClick={() => {
+                  setDown(0);
+                  setValue(option.lable);
+                }}
+              >
+                {option.lable}
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Dropdown;
