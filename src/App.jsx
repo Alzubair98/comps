e7a63acc8987092156dd5148ba5e673e79e05289
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-04-29 23:15:27 +0300

    move all thhe code to the dropdown page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-import Dropdown from "./component/Dropdown.jsx";
-import { useState } from "react";
+import DropdownPage from "./pages/DropdownPage.jsx";
 
 function App() {
-  const [value, setValue] = useState(null);
-  console.log(value);
-
-  const handleClick = (item) => {
-    setValue(item);
-  };
-
-  const options = [
-    { lable: "redddd", value: "red" },
-    { lable: "blueee", value: "blue" },
-    { lable: "greennn", value: "green" },
-  ];
   return (
     <div className="App">
-      <Dropdown options={options} value={value} onChange={handleClick} />
+      <DropdownPage />
     </div>
   );
 }
--- src/pages/DropdownPage.jsx
+++ src/pages/DropdownPage.jsx
@@ -2,7 +2,23 @@ import React, { useState } from "react";
 import Dropdown from "../component/Dropdown";
 
 const DropdownPage = () => {
-  return <div></div>;
+  const [value, setValue] = useState(null);
+  console.log(value);
+
+  const handleClick = (item) => {
+    setValue(item);
+  };
+
+  const options = [
+    { lable: "redddd", value: "red" },
+    { lable: "blueee", value: "blue" },
+    { lable: "greennn", value: "green" },
+  ];
+  return (
+    <div>
+      <Dropdown options={options} value={value} onChange={handleClick} />
+    </div>
+  );
 };
 
-export default Dropdown;
+export default DropdownPage;
