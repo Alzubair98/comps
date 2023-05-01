666383bc1cfcdd00812bb99cfcca0793b6e112d8
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-05-01 18:44:46 +0300

    finish the panel

--- src/component/Panel.jsx
+++ src/component/Panel.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import classNames from "classnames";
 
 const Panel = ({ children, className, ...rest }) => {
-  const finalClassNames = classNames("border rounded shadow");
+  const finalClassNames = classNames("border rounded shadow", className);
+
+  return <div {...rest} className={finalClassNames}></div>;
 };
 
 export default Panel;
