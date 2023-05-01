a85239937ce99fefd34437d3c631b18dfa063bdc
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-05-01 18:42:44 +0300

    identify orginal classes

--- src/component/Panel.jsx
+++ src/component/Panel.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import classNames from "classnames";
 
 const Panel = ({ children, className, ...rest }) => {
-  const finalClassNames = "";
+  const finalClassNames = classNames("border rounded shadow");
 };
 
 export default Panel;
