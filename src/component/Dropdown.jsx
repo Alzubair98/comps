22143f11cb39559b43519d7e8eb27b8106bef35b
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-05-03 22:18:00 +0300

    add useRef

--- src/component/Dropdown.jsx
+++ src/component/Dropdown.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { GoChevronDown } from "react-icons/go";
 import Panel from "./Panel";
 
