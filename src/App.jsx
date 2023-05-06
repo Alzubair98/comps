083e6cc3f5c6c2ee14f061f5a9f94a976d05aed6
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-05-06 22:11:00 +0300

    remove dropdown menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import DropdownPage from "./pages/DropdownPage.jsx";
 
 function App() {
-  return (
-    <div className="App flex">
-      <DropdownPage />
-    </div>
-  );
+  return <div className="App flex"></div>;
 }
 
 export default App;
