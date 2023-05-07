6ca1414b1545d5b3878aaa86028ca6bc2e9521aa
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-05-07 21:43:16 +0300

    finish route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Link from "./component/Link.jsx";
+import Route from "./component/Route.jsx";
+import AccordionPage from "./pages/AccordionPage.jsx";
+import DropdownPage from "./pages/DropdownPage.jsx";
 function App() {
   return (
-    <div className="App flex">
+    <div className="App ">
       <Link to="/dashborad">dashborad</Link>
+      <br />
 
       <Link to="/accordion">accordion</Link>
     </div>
