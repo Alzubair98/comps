782b3d8d46dbd5a11d834d48c19b5b2ff36e15c5
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-05-07 19:11:15 +0300

    clear naviagion Context

--- src/context/navigation.jsx
+++ src/context/navigation.jsx
@@ -15,9 +15,13 @@ const NavigationProvider = ({ children }) => {
       window.removeEventListener("popstate", handler);
     };
   }, []);
+
+  const navigate = (to) => {
+    window.history.pushState({}, "", to);
+    setCurrentPath(to);
+  };
   return (
-    <NavigationContext.Provider value={{}}>
-      {currentPath}
+    <NavigationContext.Provider value={{ currentPath, navigate }}>
       {children}
     </NavigationContext.Provider>
   );
