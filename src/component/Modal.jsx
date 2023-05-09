e3b96456f8c1d00eb885d849b00847a7e9e3dc50
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-05-09 19:29:23 +0300

    update the code in modal closing functions

--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -6,20 +6,12 @@ const Modal = ({ onClose }) => {
   return ReactDOM.createPortal(
     <div>
       <div
-        onClick={() => {
-          onClose();
-        }}
+        onClick={onClose}
         className="absolute inset-0 bg-gray-300 opacity-80"
       ></div>
       <div className="absolute inset-40 p-10 bg-white rounded ">
         im a modal{" "}
-        <Button
-          onClick={() => {
-            onClose();
-          }}
-          danger
-          rounded
-        >
+        <Button onClick={onClose} danger rounded>
           close the modal
         </Button>
       </div>
