a4d765d120c44084c737902384a5ea5b0742dce6
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-05-09 18:22:26 +0300

    create modal and modal page

---
+++ src/pages/ModalPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Modal from "../component/Modal";
+
+const ModaPage = () => {
+  return (
+    <div>
+      <Modal />
+    </div>
+  );
+};
+
+export default ModaPage;
