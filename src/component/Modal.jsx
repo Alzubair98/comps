318cc449b6aa0ddcc22ce2c34b9a775b457f73a1
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-05-09 18:49:17 +0300

    create a modal

--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -4,16 +4,19 @@ import Button from "./Button";
 const Modal = ({ onClose }) => {
   return (
     <div>
-      im a modal{" "}
-      <Button
-        onClick={() => {
-          onClose(false);
-        }}
-        danger
-        rounded
-      >
-        close the modal
-      </Button>
+      <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
+      <div className="absolute inset-40 p-10 bg-white ">
+        im a modal{" "}
+        <Button
+          onClick={() => {
+            onClose(false);
+          }}
+          danger
+          rounded
+        >
+          close the modal
+        </Button>
+      </div>
     </div>
   );
 };
