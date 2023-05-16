0d3f6f769086eae318eb581b2d689407ab30f89b
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-05-16 20:06:12 +0300

    use fragment to get rid of key error

--- src/component/Table.jsx
+++ src/component/Table.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const Table = ({ data, config }) => {
   const renderdHedaers = config.map((column) => {
     if (column.header) {
-      return column.header();
+      return <Fragment key={column.label}>{column.header()}</Fragment>;
     }
 
     return <th key={column.label}>{column.label}</th>;
