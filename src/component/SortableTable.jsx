262268468615911536b617e51df6bf1076987958
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-05-17 19:55:19 +0300

    update sortOrder

--- src/component/SortableTable.jsx
+++ src/component/SortableTable.jsx
@@ -1,10 +1,19 @@
 import Table from "./Table";
+import { useState } from "react";
 
 const SortableTable = (props) => {
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
   const { config } = props;
 
   const handleClick = (label) => {
-    console.log(label);
+    if (sortOrder === null) {
+      setSortOrder("asc");
+    } else if (sortOrder === "asc") {
+      setSortOrder("desc");
+    } else if (sortOrder === "desc") {
+      setSortOrder(null);
+    }
   };
 
   const updatedConfig = config.map((column) => {
