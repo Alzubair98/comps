2f6b1dfaf433faec6d9de970dcd488d5947d3d44
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-05-19 00:02:09 +0300

    use hook to sort data

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,26 +1,12 @@
-import React, { useState, useEffect } from "react";
+import useNumber from "../hooks/use-number";
 import Button from "../component/Button";
 
-const useNumber = (initialCount) => {
-  const [count, setCount] = useState(initialCount);
-
-  useEffect(() => {
-    console.log(count);
-  }, [count]);
-
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-
-  return { count, handleClick };
-};
-
 const CounterPage = ({ initialCount }) => {
-  const { count, handleClick } = useNumber(initialCount);
+  const { count, increment } = useNumber(initialCount);
   return (
     <div>
       <h1> Count is {count}</h1>
-      <Button onClick={handleClick} primary>
+      <Button onClick={increment} primary>
         Increment
       </Button>
     </div>
