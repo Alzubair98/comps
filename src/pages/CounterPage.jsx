99a578a13d98c3d1ff679afaa7f5109453e81bf8
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-05-19 07:30:07 +0300

    use useReducer instead of useState

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,33 +1,39 @@
 import Button from "../component/Button";
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import Panel from "../component/Panel";
 
+const reducer = (state, action) => {};
+
 const CounterPage = ({ initialCount }) => {
-  const [count, setCount] = useState(initialCount);
-  const [valueToAdd, setValueToAdd] = useState(0);
+  // const [count, setCount] = useState(initialCount);
+  // const [valueToAdd, setValueToAdd] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    valueToAdd: 0,
+  });
 
   const increment = () => {
-    setCount(count + 1);
+    //setCount(count + 1);
   };
 
   const decrement = () => {
-    setCount(count - 1);
+    //setCount(count - 1);
   };
 
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
-    setValueToAdd(value);
+    //setValueToAdd(value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setCount(count + valueToAdd);
-    setValueToAdd(0);
+    //setCount(count + valueToAdd);
+    //setValueToAdd(0);
   };
 
   return (
     <Panel className="m-3">
-      <h1 className="text-lg"> Count is {count}</h1>
+      <h1 className="text-lg"> Count is {state.count}</h1>
       <div className="flex flex-row">
         <Button onClick={increment} primary>
           Increment
@@ -39,7 +45,7 @@ const CounterPage = ({ initialCount }) => {
       <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
         <input
-          value={valueToAdd || ""}
+          value={state.valueToAdd || ""}
           onChange={handleChange}
           type="number"
           className="p-1 m-3 bg-gray-50 border border-gray-300"
